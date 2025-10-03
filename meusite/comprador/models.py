a9bb6981a68c230a5269ddfb1085c0ec00e5b561
Rodrigo Peroba de Souza <rodperoba@hotmail.com>
2025-10-03 15:58:14 -0300

    Adiciona modelo CopraProduto e implementa migrações para criação e deleção

--- meusite/comprador/models.py
+++ meusite/comprador/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+# from django.db import models
+# from produtos.models import Produto
+# from django.contrib.auth.models import User
 
-# Create your models here.
+# class CopraProduto(models.Model):
+#     id_produto = models.ForeignKey(Produto, on_delete=models.PROTECT)
+#     nome_produto = models.CharField(max_length=100)
+#     preco_produto = models.DecimalField(max_digits=10, decimal_places=2)
+#     id_comprador = models.ForeignKey(User, on_delete=models.PROTECT, related_name='comprador')
+#     id_vendedor = models.ForeignKey(User, on_delete=models.PROTECT, related_name='vendedor')
+#     data_compra = models.DateTimeField(auto_now_add=True)
+
+
+#     def __str__(self):
+#         return self.nome_produto
--- meusite/comprador/views.py
+++ meusite/comprador/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from produtos.models import Produto
+# from comprador.models import CopraProduto
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 
@@ -13,5 +14,13 @@ class ProdutoIndividual(View):
 
     def post(self, request, pk, *args, **kwargs):
         produto = Produto.objects.get(id=pk)
+        # compra = CopraProduto(
+        #     id_produto=produto,
+        #     nome_produto=produto.nome,
+        #     preco_produto=produto.preco,
+        #     id_comprador=request.user,
+        #     id_vendedor=produto.vendedor
+        # )
+        # compra.save()
         produto.delete()
         return HttpResponseRedirect(reverse_lazy('homepage'))
